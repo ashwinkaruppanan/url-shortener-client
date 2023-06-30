d21f0e659fb51548cb0682c82f25a675154b3964
ashwinkaruppanan <ashwinak148@gmail.com>
2023-07-01 01:41:45 +0530

    updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -40,7 +40,7 @@ const Dashboard = () => {
   }, []);
 
   const Loading = () => (
-    <div>
+    <div className="text-white">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
