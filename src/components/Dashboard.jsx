63612220347253af99b81f471b37ba40f2da132d
ashwinkaruppanan <ashwinak148@gmail.com>
2023-07-01 01:28:05 +0530

    updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 const Dashboard = () => {
   const navigate = useNavigate();
 
-  const [display, setDisplay] = useState("NoLinksToDisplay");
+  const [display, setDisplay] = useState("Loading");
   const [allLinksData, setAllLinksData] = useState([]);
   const [propData, setPropData] = useState();
   let reverseData = allLinksData.reverse();
@@ -28,6 +28,8 @@ const Dashboard = () => {
         if (response.data.length > 0) {
           setAllLinksData(response.data);
           setDisplay("AllLinks");
+        } else {
+          setDisplay("NoLinksToDisplay");
         }
       } catch (err) {
         console.log(err);
@@ -36,6 +38,26 @@ const Dashboard = () => {
 
     getData();
   }, []);
+
+  const Loading = () => (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 animate-spin"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+        />
+      </svg>
+      <p>Loading...</p>
+    </div>
+  );
 
   const NoLinksToDisplay = () => (
     <>
@@ -379,6 +401,9 @@ const Dashboard = () => {
   let page = "";
 
   switch (display) {
+    case "Loading":
+      page = <Loading />;
+      break;
     case "NoLinksToDisplay":
       page = <NoLinksToDisplay />;
       break;
