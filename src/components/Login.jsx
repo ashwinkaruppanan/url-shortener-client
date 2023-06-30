65fc282a7875fa0f908a91874c483465743ac251
ashwinkaruppanan <ashwinak148@gmail.com>
2023-07-01 01:53:58 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -71,6 +71,7 @@ const Login = () => {
         localStorage.setItem("user_name", response.data.full_name);
         localStorage.setItem("email", response.data.email);
         localStorage.setItem("refresh", response.data.refresh_token);
+        await new Promise((r) => setTimeout(r, 2000));
         navigate("/dashboard");
       }
     } catch (error) {
